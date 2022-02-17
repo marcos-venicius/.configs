f0a38463485807863c61fa1516cb5d6ddcdadaf8
Marcos Development <94018427+marcos-development-business@users.noreply.github.com>
2022-02-17 18:42:34 -0300

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,31 @@
+function git_branch {
+  if (Test-Path -Path ".git") {
+    Write-Host -ForegroundColor white -NoNewLine " at "
+    Write-Host -ForegroundColor green -NoNewLine "$(git rev-parse --abbrev-ref HEAD) "
+  }
+}
+
+function get_path {
+  pwd
+}
+
+function generate_prompt {
+  $path="$(pwd)"
+  $split=$path.Split("\")
+  $sector=$split[0]
+  $current_dir=$split[$split.Length - 1]
+  $long_path=$path.Split(":")[1]
+
+  Write-Host -NoNewLine "$($sector[0]) "
+  Write-Host -NoNewLine -ForegroundColor cyan "~ "
+  Write-Host -NoNewLine -ForegroundColor gray $long_path
+  git_branch
+  Write-Host -NoNewLine -ForegroundColor cyan "`n$current_dir"
+  Write-Host -NoNewLine -ForegroundColor white " [$(Get-Date -Format hh:mm)] "
+  return " - "
+}
+
+function Prompt() {
+  generate_prompt
+}
+
