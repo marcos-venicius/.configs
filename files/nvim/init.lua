b16dba45947850c6efc1b98ee82edb886601403e
Marcos Venicius <94018427+marcos-venicius@users.noreply.github.com>
2023-11-15 11:14:49 -0300

    Update init.lua

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -238,7 +238,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set('n', '<c-f>', ':NvimTreeFindFileToggle<CR>')
-vim.keymap.set('n', '<leader>i', ':silent %!prettier --stdin-filepath %<CR>')
+vim.keymap.set('n', '<leader>i', ':EslintFixAll<CR>')
 
 
 -- [[ Highlight on yank ]]
