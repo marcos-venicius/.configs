6f1f2a1d50fb25a4a1a75c1c667c12d2777944d7
Marcos Venicius <94018427+marcos-venicius@users.noreply.github.com>
2024-03-09 11:32:15 -0300

    Update configure-dev-env.sh

--- scripts/configure-dev-env.sh
+++ scripts/configure-dev-env.sh
@@ -12,13 +12,11 @@ fi
 GIT_EMAIL="$1"
 GIT_USERNAME="$2"
 
-# Install git
-
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
 sudo apt install git -y
 
-# Configure git
+# CONFIGURE GIT
 
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
@@ -40,99 +38,52 @@ git config --global alias.track "update-index --no-assume-unchanged"
 
 git config --global init.defaultBranch main
 
-# Install neovim
+# CONFIGURE NEOVIM
 
-mkdir /tmp/install-neovim
-cd /tmp/install-neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-sudo mv squashfs-root /opt/nvim
-sudo ln -s /opt/nvim/AppRun /usr/bin/nvim
-cd ~
-rm -rf /tmp/install-neovim
+sudo mkdir /opt/nvim
+mv nvim.appimage /opt/nvim
+sudo ln -s /opt/nvim/nvim.appimage /usr/bin/nv
+mkdir ~/.config/nvim
+wget https://github.com/marcos-venicius/.configs/blob/main/files/nvim/init.lua --output-document=$HOME/.config/nvim/init.lua
 
-# Install vim plug
-
-echo "INSTALLING VIM PLUG"
-
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-echo "VIM PLUG INSTALLED"
-
-# Add nvim configurations
-
-echo "CONFIGURING NVIM"
-
-cd /tmp
-
-wget https://raw.githubusercontent.com/marcos-venicius/.configs/main/files/nvim/init.vim
-
-mkdir -p ~/.config/nvim
-
-mv init.vim ~/.config/nvim/init.vim
-
-echo "NVIM CONFIGURED"
-
-# Installing tmux
+# CONFIGURE TMUX
 
 sudo apt install tmux -y
 
-# Configuring tmux
+wget https://raw.githubusercontent.com/marcos-venicius/.configs/main/files/tmux/.tmux.conf --output-document=$HOME/.tmux.conf
 
-echo "CONFIGURING TMUX"
-
-wget https://raw.githubusercontent.com/marcos-venicius/.configs/main/files/tmux/.tmux.conf
-
-mv .tmux.conf ~
-
-echo "TMUX CONFIGURED"
-
-echo "CONFIGURING ASDF"
+# CONFIGURE ASDF
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
 echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
 
-echo "ASDF CONFIGURED"
+# CONFIGURE COMMAND SHRINK
 
-echo "CONFIGURING RVM"
+cd ~ && echo "export SHRINK_TERMINAL=bash" >> ~/.bashrc && mkdir .shrink && cd .shrink && git clone https://github.com/marcos-venicius/command-shrink.git sk && echo 'sk() { ~/.shrink/sk/main.py "$@"; exec bash; }' >> ~/.bashrc
 
-sudo apt install gnupg
-gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash
-
-echo "RVM CONFIGURED"
-
-echo "CONFIGURING COMMAND SHRINK"
-
-cd ~ && echo "export SHRINK_TERMINAL=bash" >> ~/.bashrc && mkdir .shrink && cd .shrink && git clone https://github.com/marcos-venicius/command-shrink.git shrink && echo 'shrink() { ~/.shrink/shrink/cli.py "$@"; exec bash; }' >> ~/.bashrc && cd ~ && SHRINK_TERMINAL=bash shrink -help
-
-# My default shrink configurations
-# To see more access: https://github.com/marcos-venicius/command-shrink
-
-~/.shrink/shrink/cli.py st @ "git st"
-~/.shrink/shrink/cli.py cm @ "git commit -m"
-~/.shrink/shrink/cli.py all @ "git add -A"
-~/.shrink/shrink/cli.py psh @ "git push"
-~/.shrink/shrink/cli.py update @ "git fetch && git fetch --prune && git pull"
-~/.shrink/shrink/cli.py clone @ "git clone"
-~/.shrink/shrink/cli.py tl @ "tmux list-sessions"
-~/.shrink/shrink/cli.py ta @ "tmux attach-session -t"
-~/.shrink/shrink/cli.py up @ "docker compose up -d"
-~/.shrink/shrink/cli.py down @ "docker compose down"
-~/.shrink/shrink/cli.py build @ "docker compose build --no-cache"
-~/.shrink/shrink/cli.py m @ "mkdir"
-~/.shrink/shrink/cli.py lg @ "git lg"
-
-echo "COMMAND SHRINK CONFIGURED"
+~/.shrink/sk/main.py -a st "git st"
+~/.shrink/sk/main.py -a cm "git commit -m"
+~/.shrink/sk/main.py -a all "git add -A"
+~/.shrink/sk/main.py -a psh "git push"
+~/.shrink/sk/main.py -a gpull "git fetch && git fetch --prune && git pull"
+~/.shrink/sk/main.py -a clone "git clone"
+~/.shrink/sk/main.py -a tl "tmux list-sessions"
+~/.shrink/sk/main.py -a ta "tmux attach-session -t"
+~/.shrink/sk/main.py -a m "mkdir"
+~/.shrink/sk/main.py -a lg "git lg"
 
 clear
 
-echo Done!
-
-echo "Please open nvim and run :PlugInstall"
-
-cd ~
-
+echo ""
+echo "Please, close your terminal and open again"
+echo -e "\033[1;37mINSTALLED FEATURES\033[0m"
+echo ""
+echo -e "\033[1;36m  GIT\033[0m"
+echo -e "\033[1;36m  NEOVIM\033[0m"
+echo -e "\033[1;36m  TMUX\033[0m"
+echo -e "\033[1;36m  ASDF\033[0m"
+echo -e "\033[1;36m  COMMAND SHRINK\033[0m"
+echo ""
